8fe2be5d6db7ca86a2d16cabecf42034833fe801
Bhavesh Chauhan <bhavesh@xfinite.io>
2020-06-12 00:54:14 +0530

    XC-2455 Init mzaalo auth

--- app/src/main/java/com/xfinite/mzaalosdksample/MainActivity.kt
+++ app/src/main/java/com/xfinite/mzaalosdksample/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.xfinite.mzaalosdksample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.xfinite.mzaaloauth.MzaaloAuth
+import com.xfinite.mzaaloauth.MzaaloAuthInitListener
+import com.xfinite.mzaaloauth.MzaaloEnvironment
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MzaaloAuthInitListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnInitialize.setOnClickListener {
+            progress.visibility = View.VISIBLE
+
+            //Add to initialize Mzaalo Auth
+            MzaaloAuth.init(this, edtPartnerCode.text.toString(), this,getSelectedEnvironment())
+        }
+    }
+
+    private fun getSelectedEnvironment():MzaaloEnvironment{
+        return if (spinnerEnvironment.selectedItem.toString() == "STAGING") {
+            MzaaloEnvironment.STAGING
+        }else {
+            MzaaloEnvironment.PRODUCTION
+        }
+    }
+
+    override fun onError(error: String) {
+        progress.visibility = View.GONE
+        Toast.makeText(this,"Initialization failed : $error",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onSuccess() {
+        progress.visibility = View.GONE
+        Toast.makeText(this,"Initialization Successful",Toast.LENGTH_SHORT).show()
+
+        val intent = Intent(this,LoginActivity::class.java)
+        startActivity(intent)
     }
 }
